dc974cbee08e61c55d3ff587958a6796d7d3e68b
Jarrod Watts <jarrod@cubelabs.xyz>
2025-08-15 17:15:47 +1000

    Improve comments for agw-provider

--- registry/new-york/blocks/agw-provider/agw-provider.tsx
+++ registry/new-york/blocks/agw-provider/agw-provider.tsx
@@ -4,6 +4,8 @@ import { AbstractWalletProvider } from "@abstract-foundation/agw-react";
 import { QueryClient } from "@tanstack/react-query";
 import { chain } from "@/config/chain";
 
+// Learn more about Tanstack Query: https://tanstack.com/query/latest/docs/framework/react/reference/QueryClientProvider
+// We create our own query client to share our app's query cache with the AbstractWalletProvider.
 const queryClient = new QueryClient();
 
 export function NextAbstractWalletProvider({
@@ -12,6 +14,8 @@ export function NextAbstractWalletProvider({
   children: React.ReactNode;
 }) {
   return (
+    // "chain" is loaded from @config/chain and is environment-aware.
+    // i.e. use testnet or mainnet based on the environment (local, dev, prod)
     <AbstractWalletProvider chain={chain} queryClient={queryClient}>
       {children}
     </AbstractWalletProvider>
